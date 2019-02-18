6d8ee0c42271f9fa32f6ed897a68d1dd7d9e88a8
Mr-Hunter <1522138135@qq.com>
2019-02-17 19:27:29 -0800

    ss

--- Linux_UdpChatSystem/Window/window.h
+++ Linux_UdpChatSystem/Window/window.h
@@ -2,14 +2,21 @@
 
 #include <iostream>
 #include <ncurses.h>
+#define SIZE 1024
 class window
 {
 public:
   window();
+  void put_string_to_window(WINDOW* win,int y,int x,std::string& message);
+  void get_string_from_window(WINDOW* win,std::string& out_string)
   void draw_header();
   void draw_output();
   void draw_flist();
   void draw_input();
+  WINDOW* GetHeader();
+  WINDOW* GetInput();
+  WINDOW* GetFlist();
+  WINDOW* GetOutput();
   ~window();
 private:
   WINDOW *header;
